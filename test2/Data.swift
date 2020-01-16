d280b3b942381c0dfc741f86378ed53d13e883fc
ChavaFerrer <ChavaFerrer>
2020-01-16 02:37:45 -0600

    first commit

---
+++ test2/Data.swift
@@ -0,0 +1,9 @@
+//
+//  Data.swift
+//  test2
+//
+//  Created by ChavaFerrer on 14/01/20.
+//  Copyright © 2020 ChavaFerrer. All rights reserved.
+//
+
+import Foundation
---
+++ test2/animalCell.swift
@@ -0,0 +1,24 @@
+//
+//  animalCell.swift
+//  test2
+//
+//  Created by ChavaFerrer on 14/01/20.
+//  Copyright © 2020 ChavaFerrer. All rights reserved.
+//
+
+import UIKit
+
+class animalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
